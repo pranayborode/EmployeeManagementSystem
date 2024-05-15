c6c946b7bbec2c26d6f4f0f4c02db85a80fcd061
pranayborode <pranayborode@gmail.com>
2024-05-15 23:29:44 +0530

    Add project files.

---
+++ Data/Migrations/20240515133612_EmployeesDetails.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmployeeManagementSystem.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class EmployeesDetails : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmpNo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MiddleName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Country = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Depatment = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Designation = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedById = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedById = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ModifiedOn = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_employees", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "employees");
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,27 @@
+﻿namespace EmployeeManagementSystem.Models
+{
+	public class Employee : UserActivity
+	{
+		public int Id { get; set; }
+
+		public string EmpNo { get; set; }
+		public string FirstName { get; set; }
+
+		public string MiddleName { get; set; }
+		public string LastName { get; set; }
+
+		public string FullName => $"{FirstName} {MiddleName} {LastName}";
+		public string Phone { get; set; }
+		public string Email { get; set; }
+
+		public string Country { get; set; }
+
+		public DateTime DateOfBirth { get; set; }
+
+		public string Address { get; set; }
+
+		public string Depatment { get; set; }
+
+		public string Designation { get; set; }
+	}
+}
